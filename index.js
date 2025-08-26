135b9dda5e2e32ba98a5895bca0add64001ac475
pdev6373 <pdev6373@gmail.com>
2025-08-26 08:39:14 +0100

    update

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
-const express = require('express')
-require('dotenv').config()
-const cors = require('cors')
-const {connect} = require('mongoose')
-const upload = require('express-fileupload')
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const { connect } = require('mongoose');
+const upload = require('express-fileupload');
 
-const Routes = require('./routes/Routes')
-const { notFound, errorHandler } = require('./middleware/errorMiddleware')
+const Routes = require('./routes/Routes');
+const { notFound, errorHandler } = require('./middleware/errorMiddleware');
 
 const app = express();
-app.use(express.json({extended: true}));
-app.use(express.urlencoded({extended: true}));
-app.use(cors({credentials: true, origin: "http://localhost:3000"}))
-app.use(upload())
+app.use(express.json({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    credentials: true,
+    origin: ['http://localhost:3000', 'https://campus-elect.vercel.app'],
+  }),
+);
+app.use(upload());
 
-app.use('/api', Routes)
+app.use('/api', Routes);
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
-connect(process.env.MONGO_URI).then(() => app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`))).catch((error) => console.log(error))
+connect(process.env.MONGO_URI)
+  .then(() =>
+    app.listen(process.env.PORT, () =>
+      console.log(`Server running on port ${process.env.PORT}`),
+    ),
+  )
+  .catch((error) => console.log(error));
